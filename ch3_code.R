61f6ffaddaad600c71d93a7952f86bd74fe577dc
Yeowoon <woons.2016@gmail.com>
2019-03-24 22:31:52 +0900

    updated

--- ch3_code.R
+++ ch3_code.R
@@ -23,7 +23,7 @@ for (k in 1:10) {
 }
 
 # 결과에 넣어주려면 placeholder 역할 벡터가 필요
-result.final <- c()
+result.final <- NULL
 for (q in 1:10) {
     result <- q + 5
     
@@ -41,7 +41,6 @@ for (val in x) {
         count <- count+1
     }}
 
-# answer is 000??
 
 #-------------------------------------------------------------------------------------
 # while 반복문 : 조건이 참이면 반복문을 실행
@@ -80,6 +79,8 @@ if(check == 1){
     print ("check의 값은 TRUE가 아닙니다.")
 }
 
+library(tidyverse)
+
 y <- c(100, -50, 5, 3, 21, -2)
 for (t in seq_along(y)) {
     if(y[t] < 0){
@@ -101,16 +102,16 @@ ifelse(price > 50, "비싸군", "괜찮네")
 #-------------------------------------------------------------------------------------
 # function의 이해
 #-------------------------------------------------------------------------------------
-
+f(5, 10)
 f <- function(x, y){
     z <- x + y
     return (z)
 }
 
-f1 <- function(x, y){
-    z <- x + y
-    z
-}
+
+install.packages("lubridate")
+library(lubridate)
+# 1964.2.14 / "1964-02-14" / 14-02-1964
 
 # 날짜를 넣으면 요일을 반환해주는 함수 만들어보기
 dday <- function(d){
@@ -118,6 +119,7 @@ dday <- function(d){
     return(diffs)
 }
 dday("2019-02-23")
+
 mydates <- c("2018-04-25", "2017-05-24", "2002-06-12", "1999-12-31", "2018-09-12", "2005-04-15", "2011-12-25", "2007-11-23")
 
 
@@ -128,21 +130,25 @@ for(i in mydates){
 }
 
 # lapply 함수
-lapply(c("2018-04-25", "2017-05-24", "2002-06-12"), dday)
+lapply(mydates, dday) 
 
+# purr
+map(mydates, dday)
 
 #-------------------------------------------------------------------------------------
 # Tidy data : count, gather, spread
 # Tidyverse : select, filter, mutate, arrange, group_by, summarise
 #-------------------------------------------------------------------------------------
+library(tidyverse)
 
 # count 함수는 해당 변수의 value 갯수를 세어준다
 iris %>% 
     count(Species)
 
-names(mpg)
+names(mpg) / colnames(mpg)
+
 mpg %>% 
-    count(cyl, sort = T)
+    count(cyl, sort = TRUE)
 
 #-------------------------------------------------------------------------------------
 # Tame data vs Tidy data
@@ -161,7 +167,7 @@ df_tm <- tibble(
     day8 = c("결석", "결석", "출석", "출석", "결석", "출석", "출석"),
     day9 = c("결석", "출석", "출석", "출석", "결석", "결석", "출석")
 )
-
+View(df_tm2)
 # tame data -> tidy data
 df_tm2 <- gather(df_tm, "category", "value", contains("day"))
 
@@ -171,8 +177,9 @@ df_tm3 <- df_tm2 %>%
 
 # 시각화 x, y, fill 부분에 어떤 변수가 들어가있는지 확인해보자
 ggplot(df_tm3, aes(x = name, y = n, fill = fct_relevel(value, "출석", "결석"))) +
-    geom_col(position = "dodge")
-
+    geom_col(position = "dodge") +
+    theme_woons()
+ 
 #-------------------------------------------------------------------------------------
 ## 실습 데이터 가져오기
 #-------------------------------------------------------------------------------------
--- ch3_practice.R
+++ ch3_practice.R
@@ -9,11 +9,26 @@ df_sample1 <- data.frame(
     d = rnorm(10)
 )
 
+for(i in 1:4){
+    print(mean(df_sample1[, i]))
+}
+
+
 #-------------------------------------------------------------------------------------
 # practice2
 # 구구단 결과를 출력해보세요
 #-------------------------------------------------------------------------------------
+final <- NULL
+for(i in 2:9){
+    for (k in 1:9) {
+    local <- i * k
+    final <- c(final, local)
+    }
+}
 
+# 메트릭스로 변환
+final.mat <- matrix(final, nrow = 9, ncol = 8,
+                    byrow = FALSE)
 
 #-------------------------------------------------------------------------------------
 # practice3
@@ -37,8 +52,35 @@ test <- rnorm(1000)
 # 2. 컬럼이름 영문으로 변경하기
 # 3. 숫자로 변환해보기
 # 4. tidy 데이터로 변환해보기
-# 5. 다시 wide로 변환해보기 (다른 변수에 저장)
+# 5. 다시 wide로 변환해보기 (다른 변수에 저장) => 5번 빼고
 # 6. write.csv로 csv 파일로 저장하기
 # https://docs.google.com/spreadsheets/d/e/2PACX-1vQVSDG9-3h2u7_KAk4C_g4Vc50PxM1aXxqZ4w6G4f3dpdObmla4lCqDRLMZEE6bBtpTZL4lK57wkWHR/pub?gid=1581715057&single=true&output=csv
 # 혹은 budongsan.csv로 활용
 #-------------------------------------------------------------------------------------
+
+# 데이터 불러오기
+df <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQVSDG9-3h2u7_KAk4C_g4Vc50PxM1aXxqZ4w6G4f3dpdObmla4lCqDRLMZEE6bBtpTZL4lK57wkWHR/pub?gid=1581715057&single=true&output=csv")
+
+df <- read_csv("data/budongsan.csv")
+
+# 영문으로 컬럼 이름 변경
+colnames(df) <- c("gu", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+# 수치형으로 변환
+for(i in 2:6){
+    df[, i] <- as.numeric(str_remove(df[, i] %>% pull, "pt"))
+}
+
+# tame data to tidy data로 변환
+df <- df %>% 
+    gather(key = "month", value = "value", Aug : Dec)
+
+# 시각화를 위해 month 칼럼 factor에서 relevel
+month.level <- unique(df %>% pull(month))
+
+# 시각화
+ggplot(df, aes(x = fct_relevel(month, month.level), y = value, group = gu, color = gu)) +
+    geom_line() +
+    theme_woons() + # 개인테마
+    labs(x = NULL, y = NULL) +
+    geom_point(size = 1)
